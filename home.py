5a0b31190557dc09639c944b5e85621edc60905a
Sam Fox <sof5207@psu.edu>
2019-01-21 11:16:44 -0500

    edit structure

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import os
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import requests
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
